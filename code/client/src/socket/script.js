47057503bf8d6ff1a05b622f9b525129dc1bb392
alexandergm1 <alexander.1gmac@gmail.com>
2022-01-23 16:52:33 +0000

    Additional commit, issue ongoing

--- code/client/src/socket/script.js
+++ code/client/src/socket/script.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client'
 
-const socket = io('http://localhost:3000/')
+const socket = io('http://localhost:3000')
 
 
-socket.on('connect', () => {
+socket.on('connection', () => {
     console.log(`You connected with id: ${socket.id}`)
 })
--- code/server/multiplayerServer.js
+++ code/server/multiplayerServer.js
@@ -1,11 +1,8 @@
-const io = require('socket.io')(5000)
-cors: {
-    origin: ["http://localhost:3000/"]
-}
+// const io = require('socket.io')(5000)
+// cors: {
+//     origin: ["http://localhost:3000/"]
+// }
 
-io.on('connection', socket => {
-    console.log(socket.id)
-})
-
-
-
+// io.on('connection', socket => {
+//     console.log(socket.id)
+// })
