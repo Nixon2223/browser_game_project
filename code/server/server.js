5f2aed464f643a81ffece16021a7633a12beb57c
alexandergm1 <alexander.1gmac@gmail.com>
2022-01-23 15:28:15 +0000

    added socket.io package and imports

--- code/server/server.js
+++ code/server/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 const createRouter = require('./helpers/create_router');
 
 app.use(cors());
 app.use(express.json());
+
+io.on('connection', socket => {
+  console.log(socket.id)
+})
+
 
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   .then((client) => {
@@ -18,4 +25,5 @@ MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
 
 app.listen(5000, function () {
   console.log(`Listening on port ${ this.address().port }`);
-});
+
+}); 
